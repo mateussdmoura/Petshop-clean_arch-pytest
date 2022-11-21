b2a97abee0e2da1a867680bb2e012d3f27a4fe75
mateussdmoura <mateus.ssmoura@gmail.com>
2022-11-21 11:14:27 -0300

    feat: exported namedtuples to domain file to make code more readable

--- src/infra/repo/user_repo.py
+++ src/infra/repo/user_repo.py
@@ -1,6 +1,6 @@
-from collections import namedtuple
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UsersModel
 
 class UserRepository:
     """ Class to manage Users Repository """
@@ -11,15 +11,13 @@ class UserRepository:
                 - password: user password
         :return - tuple with new user inserted   
         """
-        InsertData = namedtuple('Users', 'id name, password')
-
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UsersModel(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
                 
-                return InsertData(id=new_user.id, name=new_user.name, password=new_user.password)
+                return Users(id=new_user.id, name=new_user.name, password=new_user.password)
             except:
                 db_connection.session.rollback()
                 raise
