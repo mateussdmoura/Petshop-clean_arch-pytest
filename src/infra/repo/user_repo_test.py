5caa500d704ecbf2f55c54eead7415702b44ffaa
mateussdmoura <mateus.ssmoura@gmail.com>
2022-11-21 11:00:44 -0300

    test: best practices - delete test data inserted to db after test is done

--- src/infra/repo/user_repo_test.py
+++ src/infra/repo/user_repo_test.py
@@ -17,6 +17,8 @@ def test_insert_user():
     new_user = user_repo.insert_user(name=name,password=password)
     query_user = engine.execute("SELECT * FROM users WHERE id='{}';".format(new_user.id)).fetchone()
     
+    engine.execute("DELETE FROM users WHERE id='{}';".format(new_user.id))
+    
     assert new_user.id == query_user.id
     assert new_user.name == query_user.name
     assert new_user.password == query_user.password
