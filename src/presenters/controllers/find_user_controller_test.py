7a08411ae679d992be69ab4a5f9f41454dd88b5c
mateussdmoura <mateus.ssmoura@gmail.com>
2022-11-23 13:38:51 -0300

    test: Implementing new test for find_user_controller when there is no query params

--- src/presenters/controllers/find_user_controller_test.py
+++ src/presenters/controllers/find_user_controller_test.py
@@ -20,3 +20,27 @@ def test_find_user_handle():
     # Testing inputs
     assert find_user_use_case.by_id_and_name_param["user_id"] == http_request.query["user_id"]
     assert find_user_use_case.by_id_and_name_param["name"] == http_request.query["user_name"]
+
+    # Testing outputs
+    
+    assert response.status_code == 200
+    assert response.body
+
+def test_find_user_handle_no_query_param():
+    """Testing handle method"""
+    
+    find_user_use_case = FindUserSpy(UserRepositorySpy())
+    find_user_controller = FindUserController(find_user_use_case)
+    http_request = HttpRequest()
+    
+    response = find_user_controller.handle(http_request)
+    
+    # Testing inputs
+    assert find_user_use_case.by_id_and_name_param == {}
+    assert find_user_use_case.by_id_param == {}
+    assert find_user_use_case.by_name_param == {}    
+
+    # Testing outputs
+    
+    assert response.status_code == 400
+    assert "error" in response.body
